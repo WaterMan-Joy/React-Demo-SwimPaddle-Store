1647d4326570bddad8dd23373da3edd17754dd66
WaterMan-Joy <nietzsche21st@gmail.com>
2022-12-09 18:35:34 +0900

    더미데이터 추가

---
+++ src/components/SwimPaddles/PaddleSummary.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./PaddleSummary.module.css";
+
+const PaddleSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Paddle</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa enim
+        quis corrupti. Culpa id nemo quo, in ipsa explicabo autem quas sit
+        quaerat, dolor, reprehenderit necessitatibus velit placeat ad.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident natus
+        aut sint quas architecto magnam voluptatem nesciunt, ea in delectus
+        laborum vero rem eaque dolorem reprehenderit. Pariatur sapiente aperiam
+        aliquid?
+      </p>
+    </section>
+  );
+};
+
+export default PaddleSummary;
